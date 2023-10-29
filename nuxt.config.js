9123fd34516627b973534162cf051bad9324dc08
Solomon Eseme <kaperskyguru@gmail.com>
2023-10-29 08:18:50 +0100

    fix: update redirection

--- nuxt.config.js
+++ nuxt.config.js
@@ -378,7 +378,7 @@ export default {
     },
 
     {
-      from: '^academy.masteringbackend.com(.*)$',
+      from: 'http://academy.masteringbackend.com(.*)$',
       to: '/academy',
       statusCode: 301,
     },
