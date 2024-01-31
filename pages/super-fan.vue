97a7c4c2d28a60122152976861f52f3547a2ed32
Solomon Eseme <kaperskyguru@gmail.com>
2024-01-31 17:38:15 +0100

    fix: SEO

--- pages/super-fan.vue
+++ pages/super-fan.vue
@@ -442,6 +442,37 @@ export default {
       )
     },
   },
+
+  head() {
+    return {
+      title: 'Become a Super Fan',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'A great resource for backend engineers. Next-level Backend Engineering training and Exclusive resources.',
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content:
+            'A great resource for backend engineers. Next-level Backend Engineering training and Exclusive resources.',
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            'A great resource for backend engineers. Next-level Backend Engineering training and Exclusive resources.',
+        },
+        {
+          hid: 'twitter:card',
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+      ],
+    }
+  },
 }
 </script>
   
