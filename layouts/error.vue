a4215f551bed5c9e04afa4139de2133f5c8c1b76
Solomon Eseme <kaperskyguru@gmail.com>
2024-05-21 02:16:27 +0100

    feat: error page added

---
+++ layouts/error.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    style="
+      background-color: #f7f5fa;
+      width: 100% !important;
+      min-height: auto;
+      margin: 20rem 0px;
+    "
+  >
+    <div v-if="error.statusCode === 404">
+      <div class="page-wrap d-flex flex-row align-items-center">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+              <span class="display-1 d-block">404</span>
+              <div class="mb-4 lead">
+                The page you are looking for was not found.
+              </div>
+              <a href="/" class="btn btn-link">Back to Home</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h1 v-if="error.statusCode !== 404">
+      <div class="page-wrap d-flex flex-row align-items-center">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+              <span class="display-1 d-block">500</span>
+              <div class="mb-4 lead">
+                An error occurred, please reload the page
+              </div>
+              <a href="/" class="btn btn-link">Back to Home</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </h1>
+  </div>
+</template>
+  
+  <script>
+export default {
+  name: 'errorPage',
+  layout: 'error',
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
